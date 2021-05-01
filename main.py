83e736065bd172e38d1b2f55dbeac47ed0df38ef
adkevin3307 <adkevin3307@gmail.com>
2021-05-01 15:04:18 +0800

    update args

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from utils import parse, show_history
-from DataLoader import load_data
-from Net import ResNet
+from utils import parse, load_net, load_data, show_history
 from Model import Model
 
 if __name__ == '__main__':
@@ -17,9 +15,9 @@ if __name__ == '__main__':
 
     args = parse()
 
-    train_set, test_set, train_loader, test_loader = load_data(args.root, args.csv)
+    train_loader, test_loader = load_data(args.root, args.csv, args.shape)
 
-    net = ResNet(pretrained=args.pretrained)
+    net = load_net(args.net, args.pretrained)
 
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
     criterion = nn.CrossEntropyLoss()
@@ -27,6 +25,6 @@ if __name__ == '__main__':
     model = Model(net, optimizer, criterion)
 
     train_history = model.train(train_loader, epochs=args.epochs, val_loader=test_loader)
-    test_history = model.test(test_loader)
+    # test_history = model.test(test_loader)
 
-    show_history(train_history)
+    # show_history(train_history)
